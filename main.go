ccf2bbc5e61456f15307e3fc919083e1f1d4b041
TuxedoFish <harryliversedge@gmail.com>
2021-06-27 14:10:58 +0100

    learn to output files

--- main.go
+++ main.go
@@ -2,21 +2,52 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	"github.com/TuxedoFish/golang-learning/src/simple"
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
-	doSimple()
+	sm := doSimple()
+
+	writeToFile("simple.bin", sm)
+	// readFromFile()
 }
 
-func doSimple() {
+// Writing a protobuffer to file
+func writeToFile(fname string, pb proto.Message) error {
+	// Create output for protobuffer
+	out, err := proto.Marshal(pb)
+	// Check if any error
+	if err != nil {
+		log.Fatalln("Can't serialize to bytes")
+		return err
+	}
+
+	if err := ioutil.WriteFile(fname, out, 0644); err != nil {
+		log.Fatalln("Can't write to file", err)
+	}
+
+	fmt.Println("Data has been written!")
+	return nil
+}
+
+// Returns a pointer to a simple message
+func doSimple() *simple.SimpleMessage {
 	sm := simple.SimpleMessage{
 		Id:         1234,
 		IsSimple:   true,
 		Name:       "My Simple Message",
 		SampleList: []int32{1, 3, 5, 7},
 	}
-
 	fmt.Println(sm)
+
+	sm.Name = "I have been renamed"
+	fmt.Println(sm)
+
+	fmt.Println(sm.GetId())
+
+	return &sm
 }
