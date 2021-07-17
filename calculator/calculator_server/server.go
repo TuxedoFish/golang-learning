32c3d679b5fa6948fdbc91e3c24835544096bf16
TuxedoFish <harryliversedge@gmail.com>
2021-07-17 18:00:27 +0100

    add calculator service

---
+++ calculator/calculator_server/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/TuxedoFish/golang-learning/calculator/calculatorpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+	fmt.Printf("Sum function was invoked with: %v", req)
+	result := req.GetA() + req.GetB()
+	res := calculatorpb.SumResponse{
+		Result: result,
+	}
+	return &res, nil
+}
+
+func main() {
+	fmt.Println("Starting calculator service")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+
+	if err != nil {
+		log.Fatalf("Failed to listed: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	calculatorpb.RegisterSumServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
